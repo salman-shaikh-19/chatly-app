658355c3b3c563bf923f926591e4099b86c294f6
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-03 12:19:22 +0530

    UserItem.jsx

---
+++ src/features/chat/components/UserItem.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import dayjs from "dayjs";
+
+function UserItem({ userId, name,avatar,online, isTyping,lastMsgId, lastMsgText,lastMsgSender,lastMsgTime,loggedInUserId,
+  selectChat,}) {
+  return (
+    <div
+      className="flex items-center px-3 py-2 border-b cursor-pointer hover:bg-gray-200"
+      onClick={() => selectChat({ id: userId, name, avatar })}
+    >
+      <div className="relative w-12 h-12">
+        <img
+          src={avatar ?? "https://ncetir.com/Images/login@4x.png"}
+          className="object-cover w-12 h-12 rounded-full"
+        />
+        <span
+          className={`absolute bottom-0 right-0 w-3 h-3 rounded-full border-2 border-white ${
+            online ? "bg-green-500" : "bg-gray-400"
+          }`}
+        ></span>
+      </div>
+
+      <div className="w-full ml-2">
+        <div className="flex justify-between">
+          <span className="font-semibold text-gray-600">{name}</span>
+          <span className="text-sm text-gray-500">
+            {lastMsgTime ? dayjs(lastMsgTime).format("hh:mm A") : ""}
+          </span>
+        </div>
+
+        <span className="block text-sm text-gray-600 truncate w-50">
+          {isTyping
+            ? "Typing..." : lastMsgText
+            ? `${lastMsgSender === loggedInUserId ? "You" : name}: ${lastMsgText}`
+            : "No messages yet"}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+// only re-render if relevant props change
+export default React.memo(UserItem, (prev, next) => {
+  return (
+    prev.userId === next.userId &&
+    prev.online === next.online &&
+    prev.isTyping === next.isTyping &&
+    prev.lastMsgId === next.lastMsgId && // message changed only if id changes
+    prev.lastMsgTime === next.lastMsgTime
+  );
+});
