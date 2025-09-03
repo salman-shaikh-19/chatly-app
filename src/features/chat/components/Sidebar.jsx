b838dab2f4f7555243cec02f2b47e358811e58a2
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-03 18:53:33 +0530

    Sidebar.jsx

--- src/features/chat/components/Sidebar.jsx
+++ src/features/chat/components/Sidebar.jsx
@@ -1,8 +1,8 @@
-import { faSignOutAlt, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faSignOutAlt, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CustomInfiniteScroll from "../../../common/components/CustomInfiniteScroll";
 import { faComments } from "@fortawesome/free-regular-svg-icons";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { getChatId } from "../../../common/utils/getChatId";
 import _ from "lodash";
@@ -13,15 +13,34 @@ const Sidebar = ({ users, userListLoading,logoutFunction,selectChat,onlineUsers,
   lastMessages, }) => {
   const pageSize = 10;
   const isTyping = useSelector((state) => state.common.isTyping || {});
+  const [searchText,setSearchText]=useState("");
+  
 
+   // useRef to keep the debounced function persistent
+  const handleSearch = useRef(
+    _.debounce((value) => {
+      setSearchText(value.trim());
+      // users= users.filter(user=>user.name.toLowerCase()==value.toLowerCase());
+    }, 500)
+  ).current;
+
+  useEffect(() => {
+    return () => handleSearch.cancel(); // cancel pending debounced calls on unmount
+  }, []);
+  // console.log(searchText);
+  
+    
   // Sort users by latest message timestamp
 const sortedUsers = useMemo(() => {
-  return _.orderBy(users, (u) => {
+  return _.orderBy(
+    // users
+     users.filter(u => u.name.toLowerCase().includes(searchText.toLowerCase()))
+    , (u) => {
     const chatId = getChatId(loggedInUserId, u.id);
     const ts = lastMessages[chatId]?.timestamp;
     return ts ? new Date(ts).getTime() : 0;
   }, ["desc"]);
-}, [users, loggedInUserId, Object.keys(lastMessages).length]);
+}, [users, loggedInUserId, Object.keys(lastMessages).length,searchText]);
 const loggedInUser = users.find(u => u.id === loggedInUserId);
 
   return (
@@ -33,6 +52,7 @@ const loggedInUser = users.find(u => u.id === loggedInUserId);
         <h2 className="text-white">
           <FontAwesomeIcon icon={faComments} size="2x" /> Chatly
         </h2>
+        
            <ProfileDropdown
            avatar={loggedInUser?.avatar}
          username={loggedInUser?.name || "User"}
@@ -41,7 +61,7 @@ const loggedInUser = users.find(u => u.id === loggedInUserId);
        
       </div>
 
-      {userListLoading ? (
+      {userListLoading  ? (
         <div className="flex justify-center text-gray-600 items-center h-full">
           <FontAwesomeIcon icon={faSpinner} spin size="2x" />
         </div>
@@ -50,17 +70,37 @@ const loggedInUser = users.find(u => u.id === loggedInUserId);
           data={sortedUsers}
           pageSize={pageSize}
           scrollTargetId="user-scroll"
-          endMsg="No more users"
+          endMsg=""
         >
           {(items) => (
             <div className="flex flex-col">
-              {/* <strong className="text-gray-800 ms-2">Chats</strong> */}
-              {items
+              
+                <div className="relative m-2">
+                    <span className="absolute inset-y-0 left-2 flex items-center text-gray-500">
+                      <FontAwesomeIcon icon={faSearch} />
+                    </span>
+                      <input
+                        type="search"
+                        // value={searchText}
+                        placeholder="Search..."
+                        className="pl-8 pr-2 py-1 w-full text-gray-900 placeholder:text-gray-500 bg-gray-300  border border-gray-300 outline-none transition duration-150 
+                          rounded-full"
+
+                          onChange={(e)=>handleSearch(e.target.value)}
+                      />
+                </div>
+
+ {/* <strong className="text-gray-800 ms-2">Chats</strong> */}
+ {
+  items.length==0 ? (
+    <div className="text-center text-gray-500">No user found</div>
+  )
+     :   items
                 .filter((item) => item.id !== loggedInUserId)
                 .map((user) => {
                   const chatId = getChatId(loggedInUserId, user.id);
                   return (
-               
+                 
                     <UserCard
                     key={user.id}
                     userId={user.id}
