d836977732b0f265bf1829856bbb6e044a6aa4aa
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-03 17:29:17 +0530

    ProfileDropdown.jsx

--- src/features/user/components/ProfileDropdown.jsx
+++ src/features/user/components/ProfileDropdown.jsx
@@ -22,14 +22,14 @@ const ProfileDropdown = ({ username,avatar, logoutFunction }) => {
     <div className="relative" ref={dropdownRef}>
       <button
         onClick={() => setOpen((prev) => !prev)}
-        className="flex items-center gap-2 px-3 py-2 bg-teal-950 rounded hover:bg-teal-900 focus:outline-none"
+        className="flex items-center gap-2  py-0 bg-teal-950 rounded hover:bg-teal-950 hover:cursor-pointer focus:outline-none"
       >
-     
-      <CommonAvatar
+    <CommonAvatar
       avatar={avatar}
+      avatarClassName="h-10 w-10"
       />
         
-        {/* <FontAwesomeIcon icon={faCaretDown} /> */}
+        {/* <FontAwesomeIcon icon={faUser} /> */}
       </button>
 {open && (
   <div className="absolute right-0 mt-2 w-40 bg-white border rounded shadow-lg z-50">
