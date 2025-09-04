16c0175c5d7aa591783950d3387ea4801258b60c
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-04 11:25:12 +0530

    NetworkStatus.jsx

--- src/common/components/NetworkStatus.jsx
+++ src/common/components/NetworkStatus.jsx
@@ -18,12 +18,12 @@ export default function NetworkStatus() {
     };
   }, []);
 
-  if (!isOnline) return null; // nothing when online 
+  if (isOnline) return null; // nothing when online 
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70 ">
       <div className="bg-red-600 text-white px-6 py-4 rounded-lg shadow-lg text-center max-w-md mx-10">
-        <FontAwesomeIcon icon={faWarning} fade className="text-3xl mb-2" />
+{/*         <FontAwesomeIcon icon={faWarning} fade className="text-3xl mb-2" /> */}
         <h2 className="text-xl font-bold mb-2">You are offline</h2>
         <p className="text-sm"><FontAwesomeIcon icon={faWarning}  /> Check your internet connection to continue using the app.</p>
       </div>
