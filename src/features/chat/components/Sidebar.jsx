282640c6afca5dd9b14ed94158b9ad0b2818870d
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-04 18:09:20 +0530

    Sidebar.jsx

--- src/features/chat/components/Sidebar.jsx
+++ src/features/chat/components/Sidebar.jsx
@@ -45,7 +45,7 @@ const loggedInUser = users.find(u => u.id === loggedInUserId);
 
   return (
     <div
-      className="w-100 max-w-full h-screen hide-scrollbar bg-gray-100 text-white overflow-auto"
+      className="w-90 max-w-full h-screen hide-scrollbar bg-gray-100 text-white overflow-auto"
       id="user-scroll"
     >
       <div className="bg-teal-950 p-5 text-sm md:text-base lg:text-lg flex justify-between">
