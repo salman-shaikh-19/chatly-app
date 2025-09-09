0de0df29aebf0ff80708a3b55db225280e15180e
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-09 15:51:16 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import AppRoutes from "./routes"
 
 
 function App() {
+// register service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => {
+      console.log("Service Worker registered:", reg);
+    })
+    .catch((err) => console.error("SW registration failed:", err));
+}
 
   return (
     <>
