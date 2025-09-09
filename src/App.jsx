b4b7ca32bddb48dea8da14d26c9e4efbab1662cc
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-09 16:12:33 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/sw.js")
     .then((reg) => {
-      console.log("Service Worker registered:", reg);
+      // console.log("Service Worker registered:", reg);
     })
     .catch((err) => console.error("SW registration failed:", err));
 }
