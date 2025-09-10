5f284d093efbc870bb6b19ef99789959882516f0
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-10 18:25:08 +0530

    Update NoMsgYet.jsx

--- src/common/components/NoMsgYet.jsx
+++ src/common/components/NoMsgYet.jsx
@@ -2,7 +2,7 @@ import ChatCommonLabel from "./ChatCommonLabel"
 
 const NoMsgYet = () => {
   return (
-    <div className="flex items-center justify-center ">
+    <div className="flex items-center select-none justify-center ">
       <ChatCommonLabel>
         No Messages Yet
       </ChatCommonLabel>
