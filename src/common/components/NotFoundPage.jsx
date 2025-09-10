6c3d2e81c529b77b25848b6053ad0e7c61417d1a
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-10 18:25:48 +0530

    Update NotFoundPage.jsx

--- src/common/components/NotFoundPage.jsx
+++ src/common/components/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 
 const NotFoundPage = () => {
     return (
-        <div style={{ padding: "2rem", textAlign: "center" }}>
+        <div className="select-none" style={{ padding: "2rem", textAlign: "center" }}>
             <h1>404 - Page Not Found</h1>
             <p>Sorry, the page you are looking for does not exist.</p>
         </div>
