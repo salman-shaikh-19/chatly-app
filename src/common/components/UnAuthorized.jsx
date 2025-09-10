e1e4c6d697e0d1269c1f211517bf60f9d1f45100
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-10 18:26:19 +0530

    Update UnAuthorized.jsx

--- src/common/components/UnAuthorized.jsx
+++ src/common/components/UnAuthorized.jsx
@@ -1,7 +1,7 @@
 
 const UnAuthorized = () => {
     return (
-        <div style={{ padding: "2rem", textAlign: "center" }}>
+        <div className="select-none" style={{ padding: "2rem", textAlign: "center" }}>
             <h1>401 - Unauthorized</h1>
             <p>Sorry, you dont have an access to this page.</p>
         </div>
