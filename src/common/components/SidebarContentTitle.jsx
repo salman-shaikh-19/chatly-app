bb9793669cde384687b9bf6c40252eea779248d3
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-11 11:26:45 +0530

    Update SidebarContentTitle.jsx

--- src/common/components/SidebarContentTitle.jsx
+++ src/common/components/SidebarContentTitle.jsx
@@ -6,14 +6,15 @@ const SidebarContentTitle = ({ contentTitle, length, onclick, isExpand }) => {
     return (
         <>
             {length > 0 && (
-                <strong onClick={onclick} title={isExpand ? `Collapse ${contentTitle}` : `Expand ${contentTitle}`} className="text-gray-700 select-none mb-1 ml-1">
+                <strong onClick={onclick}  className="text-gray-700 select-none mb-1 ml-1">
 
-                    <div className="flex items-center gap-1 cursor-pointer">
+                    <div className="flex items-center gap-1 cursor-pointer" >
                         <FontAwesomeIcon
                             icon={isExpand ? faChevronDown : faChevronRight}
                             className="text-gray-600"
+                            
                         />
-                        <span> {contentTitle}
+                        <span title={isExpand ? `Collapse ${contentTitle}` : `Expand ${contentTitle}`}> {contentTitle}
                             {length > 1 ? "s" : ""}
                              <span className="ml-1 text-xs text-gray-500">({length})</span>
                             </span>
