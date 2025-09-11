c6b10ec7d9be21476301c2ba8be539feda228aa1
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-11 10:54:00 +0530

    Update SidebarContentTitle.jsx

--- src/common/components/SidebarContentTitle.jsx
+++ src/common/components/SidebarContentTitle.jsx
@@ -1,12 +1,21 @@
+import { faChevronDown, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
 
-const SidebarContentTitle = ({ contentTitle, length }) => {
+const SidebarContentTitle = ({ contentTitle, length, onclick, isExpand }) => {
     return (
         <>
             {length > 0 && (
-                <strong className="text-gray-700 select-none mb-1 ml-1">
-                    {contentTitle}
-                    {length > 1 ? "s" : ""}
+                <strong onClick={onclick} className="text-gray-700 select-none mb-1 ml-1">
+
+                    <div className="flex items-center gap-1 cursor-pointer">
+                        <FontAwesomeIcon
+                            icon={isExpand ? faChevronDown : faChevronRight}
+                            className="text-gray-600"
+                        />
+                        <span> {contentTitle}
+                            {length > 1 ? "s" : ""}</span>
+                    </div>
                 </strong>
             )
             }
