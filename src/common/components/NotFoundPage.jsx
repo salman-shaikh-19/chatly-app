ddcd98c0cdcad9e54020bfb215ae4963eb1574bc
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-11 17:37:56 +0530

    Update NotFoundPage.jsx

--- src/common/components/NotFoundPage.jsx
+++ src/common/components/NotFoundPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import NoFound from '../../assets/images/fallback/404-img.png';
 
 const NotFoundPage = () => {
@@ -10,8 +11,9 @@ const NotFoundPage = () => {
             />
             {/* <h1 className="text-2xl md:text-3xl font-bold mt-4">404 - Page Not Found</h1>
             <p className="text-gray-500 mt-2 text-sm md:text-base">
-                Sorry, the page you are looking for does not exist.
+            Sorry, the page you are looking for does not exist.
             </p> */}
+            <Link to="/" className='bg-teal-700 w-50 mb-10 text-white px-4 py-2 rounded-2xl'>Go Back To Home Page</Link>
         </div>
     );
 };
