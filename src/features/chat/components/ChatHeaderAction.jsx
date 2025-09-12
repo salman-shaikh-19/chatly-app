d42f85708bead9af04bba2a4bf72c3789707abcb
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-12 11:54:48 +0530

    Create ChatHeaderAction.jsx

---
+++ src/features/chat/components/ChatHeaderAction.jsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const commonActionStyle = "text-white border p-1 rounded-sm hover:text-gray-500 cursor-pointer";
+
+
+const ChatHeaderAction = ({ 
+  className = "", onClick, 
+  title = "default title", 
+  icon, children 
+}) => {
+  return (
+    <div className={`${commonActionStyle} ${className}`}
+      onClick={onClick}  title={title} >
+      <FontAwesomeIcon icon={icon} />
+      {children && <span className="ml-1">{children}</span>}
+    </div>
+  );
+};
+
+export default React.memo(ChatHeaderAction);
