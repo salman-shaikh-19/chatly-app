46e7dadad42ea91f951651c0959e06a1f641d8f6
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-12 17:48:12 +0530

    Update ChatHeader.jsx

--- src/features/chat/private_chat/ChatHeader.jsx
+++ src/features/chat/private_chat/ChatHeader.jsx
@@ -5,17 +5,17 @@ import CommonAvatar from "../../user/components/CommonAvatar";
 import ChatHeaderAction from "../components/ChatHeaderAction";
 const ChatHeader = ({ selectedChatUser, onlineUsers, handleDeleteAll, selectedMsgs, handleSelectedDelete, messages, goBack, typing }) => {
     const userId = selectedChatUser?.id;
-    const isUserOnline=onlineUsers.includes(selectedChatUser.id);
+
     return (
         <div className="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 bg-teal-950"
         >
             <div className="relative w-12 h-12">
                 <CommonAvatar
                     avatar={selectedChatUser.avatar}
-                    avatarClassName={`h-12 w-12 p-0.5 ${isUserOnline ? 'bg-green-800' : 'bg-red-700' }`}
+                    avatarClassName="h-12 w-12 "
                 />
                 <span
-                    className={`absolute bottom-1 right-0 w-3 h-3 rounded-full border-2
+                    className={`absolute bottom-0 right-0 w-3 h-3 rounded-full border-2
                  border-white ${onlineUsers.includes(selectedChatUser?.id)
                             ? "bg-green-500" : "bg-gray-400"
                         }`}
@@ -55,7 +55,7 @@ const ChatHeader = ({ selectedChatUser, onlineUsers, handleDeleteAll, selectedMs
 
                 </div>
                 <span className="text-gray-400 mx-2 ">
-                    {typing ? `${selectedChatUser.name} Typing.....` : isUserOnline ? 'Online' : 'Offline'}</span>
+                    {typing ? `${selectedChatUser.name} Typing.....` : onlineUsers.includes(selectedChatUser.id) ? 'Online' : 'Offline'}</span>
             </div>
         </div>
     )
