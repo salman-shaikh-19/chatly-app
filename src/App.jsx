924fc442637d5e53058f239b04490e9bffeecddc
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-12 18:14:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { useEffect } from "react";
 import NetworkStatus from "./common/components/NetworkStatus"
 import AppRoutes from "./routes"
+import { toast } from "react-toastify";
 
 
 function App() {
-// register service worker
-if ("serviceWorker" in navigator) {
+  // useEffect(()=>{
+ useEffect(() => {
+    const handleKeyDown = (e) => {
+      // Block PrintScreen key
+      if (e.key === "PrintScreen") {
+        toast.warning("Screenshots are disabled!");
+        e.preventDefault();
+      }
+
+      // Block Ctrl+P (print)
+      if (e.ctrlKey && e.key === "p") {
+         toast.warning("Printing is disabled!");
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, []);
+    // register service worker
+ if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/sw.js")
     .then((reg) => {
@@ -13,10 +34,12 @@ if ("serviceWorker" in navigator) {
     .catch((err) => console.error("SW registration failed:", err));
 }
 
+  // },[])
+
   return (
     <>
     <NetworkStatus />
-   <AppRoutes />
+    <AppRoutes />
     </>
   )
 }
