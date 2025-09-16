0f8f2a0e8de014e78a18a455b3e4df8f5858a93c
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-16 12:46:04 +0530

    Update GroupChatMessages.jsx

--- src/features/chat/group_chat/GroupChatMessages.jsx
+++ src/features/chat/group_chat/GroupChatMessages.jsx
@@ -127,18 +127,15 @@
 // };
 
 // export default React.memo(GroupChatMessages);
-
 import React, { useCallback, useMemo } from "react";
 import GroupChatMessage from "./GroupChatMessage";
 import dayjs from "dayjs";
-
 
 import isEditOrDeletable from "../../../common/utils/isEditOrDeletable";
 import { useSelector } from "react-redux";
 import ChatCommonLabel from "../../../common/components/ChatCommonLabel";
 import ChatCommonDateLabel from "../../../common/components/ChatCommonDateLabel";
 import NoMsgYet from "../../../common/components/NoMsgYet";
-
 
 const GroupChatMessages = ({
   messages,
@@ -150,146 +147,122 @@ const GroupChatMessages = ({
   typingUserNames,
   handleDeleteMessage,
   handleEditMsg,
-  currentGroup
+  currentGroup,
 }) => {
+  const toggleSelect = useCallback(
+    (id) => {
+      setSelectedMsgs((prev) =>
+        prev.includes(id) ? prev.filter((x) => x !== id) : [...prev, id]
+      );
+    },
+    [setSelectedMsgs]
+  );
 
-  const toggleSelect = useCallback((id) => {
-    setSelectedMsgs((prev) =>
-      prev.includes(id) ? prev.filter((x) => x !== id) : [...prev, id]
-    );
-  }, [setSelectedMsgs]);
-  const { users } = useSelector(state => state.user);
-  // console.log(currentGroup);
-const usersMap = useMemo(() => {
-  const map = new Map();
-  users.forEach(u => map.set(u.id, u));
-  return map;
-}, [users]);
-// console.log(currentGroup);
+  const { users } = useSelector((state) => state.user);
+
+  const usersMap = useMemo(() => {
+    const map = new Map();
+    users.forEach((u) => map.set(u.id, u));
+    return map;
+  }, [users]);
+
   const creatorId = currentGroup ? Number(currentGroup.createdBy) : null;
-  
+
+  // ✅ Combine messages and "user added" events
+  const combinedEvents = useMemo(() => {
+    if (!currentGroup) return [];
+
+    const addedEvents = currentGroup.groupUsers
+      .filter((u) => Number(u.userId) !== creatorId)
+      .map((u) => ({
+        type: "added",
+        userId: u.userId,
+        timestamp: u.joinedAt,
+      }));
+
+    const msgEvents = messages.map((m) => ({ type: "message", message: m }));
+
+    const allEvents = [...addedEvents, ...msgEvents];
+
+    allEvents.sort(
+      (a, b) => dayjs(a.timestamp).unix() - dayjs(b.timestamp).unix()
+    );
+
+    return allEvents;
+  }, [currentGroup, messages, creatorId]);
+
   return (
     <div className="flex-1 overflow-y-auto p-2 flex flex-col space-y-2 hide-scrollbar">
-      {
-        currentGroup && (
-          <div className="text-xs flex flex-col items-center justify-center">
-            <ChatCommonLabel>
-              {currentGroup?.groupName} was created on {dayjs(currentGroup?.createdAt).format("MMM D, YYYY")} By {creatorId==Number(loggedInUserId) ? "You" : (usersMap.get(creatorId)?.name || "Unknown")}
+      {currentGroup && (
+        <div className="text-xs flex flex-col items-center justify-center">
+          <ChatCommonLabel>
+            {currentGroup?.groupName} was created on{" "}
+            {dayjs(currentGroup?.createdAt).format("MMM D, YYYY")} By{" "}
+            {creatorId === Number(loggedInUserId)
+              ? "You"
+              : usersMap.get(creatorId)?.name || "Unknown"}
+          </ChatCommonLabel>
+        </div>
+      )}
 
-            </ChatCommonLabel>
+      {combinedEvents.length === 0 && <NoMsgYet />}
 
+      {combinedEvents.map((event, index) => {
+        const prevEvent = index > 0 ? combinedEvents[index - 1] : null;
+        const showDateBadge =
+          !prevEvent ||
+          !dayjs(prevEvent.timestamp).isSame(event.timestamp, "day");
 
-            {
-              currentGroup.groupUsers.map((user, i) => {
+        return (
+          <React.Fragment key={index}>
+            {showDateBadge && (
+              <div className="text-xs flex items-center justify-center">
+                <ChatCommonLabel>
+                  <ChatCommonDateLabel timestamp={event.timestamp} />
+                </ChatCommonLabel>
+              </div>
+            )}
 
-                const userObj = usersMap.get(Number(user.userId));
-                // console.log(userObj);
-                    if (!userObj) return null;
-                 if (Number(user.userId) === creatorId) return null;
+            {event.type === "added" && (
+              <div className="text-xs flex flex-col items-center justify-center">
+                <ChatCommonLabel>
+                  {usersMap.get(Number(event.userId))?.name} was added By{" "}
+                  {creatorId === Number(loggedInUserId)
+                    ? "You"
+                    : usersMap.get(creatorId)?.name || "Unknown"}
+                </ChatCommonLabel>
+              </div>
+            )}
 
-                return (
-                  // <span key={i} className="text-gray-500 my-1 px-3 py-1 rounded-md bg-white">
-                  //   {userObj?.name} was added on {dayjs(user.joinedAt).format("MMM D, YYYY")}
-                  // </span>
-                
-          !dayjs(user.joinedAt).isAfter(dayjs(currentGroup?.createdAt), "day")
-                  &&
-                  <ChatCommonLabel key={i}>
-                    {userObj?.name} was added on {dayjs(user.joinedAt).format("MMM D, YYYY")}
-                  </ChatCommonLabel>
-                
-                  
-                  
-
-                )
-              })
-            }
-          </div>
-        )
-      }
-      {messages.length > 0 ? (
-        messages.map((msg, index) => {
-          const prevMsg = index > 0 ? messages[index - 1] : null;
-          const showDateBadge = !prevMsg || !dayjs(prevMsg.timestamp).isSame(msg.timestamp, "day");
-          const isMsgEditAndDeletable = isEditOrDeletable(msg.timestamp);
-          return (
-            <React.Fragment key={`group-msg-${msg.messageId}-${index}`}>
-              {showDateBadge && (
-                <>
-                <div className="text-xs flex items-center justify-center">
-                  <ChatCommonLabel>
-                   
-                   <ChatCommonDateLabel timestamp={msg.timestamp}/>
-              
-                  </ChatCommonLabel>
-                  
-
-                </div>
-             
-            {
-              currentGroup.groupUsers.map((user, i) => {
-                // console.log(currentGroup);
-                
-                const userObj = usersMap.get(Number(user.userId));
-                // console.log(userObj);
-                    if (!userObj) return null;
-                 if (Number(user.userId) === creatorId) return null;
-
-                return (
-                  // <span key={i} className="text-gray-500 my-1 px-3 py-1 rounded-md bg-white">
-                  //   {userObj?.name} was added on {dayjs(user.joinedAt).format("MMM D, YYYY")}
-                  // </span>
-                
-                  // dayjs(user.joinedAt).isAfter(dayjs(currentGroup?.createdAt), "day")
-                  !dayjs(currentGroup?.createdAt).isSame(dayjs(user.joinedAt)) && 
-                  dayjs(user.joinedAt).isSame(dayjs(msg.timestamp), "day")
-
-                  &&
-                    <div className="text-xs flex flex-col items-center justify-center">
-                  <ChatCommonLabel key={i}>
-                    {userObj?.name} was added By {creatorId==Number(loggedInUserId) ? "You" : (usersMap.get(creatorId)?.name || "Unknown")}
-
-                  </ChatCommonLabel>
-                </div>
-                  
-                  
-
-                )
-              })
-            }
-                </>
-                
-              )}
-
+            {event.type === "message" && (
               <GroupChatMessage
-                msg={msg}
-                isEditableAndDeletable={isMsgEditAndDeletable}
-                isOwnMessage={msg.senderId === loggedInUserId}
-                isSelected={selectedMsgs.includes(msg.messageId)}
-                toggleSelect={() => toggleSelect(msg.messageId)}
+                msg={event.message}
+                isEditableAndDeletable={isEditOrDeletable(
+                  event.message.timestamp
+                )}
+                isOwnMessage={event.message.senderId === loggedInUserId}
+                isSelected={selectedMsgs.includes(event.message.messageId)}
+                toggleSelect={() => toggleSelect(event.message.messageId)}
                 handleDeleteMessage={handleDeleteMessage}
                 handleEditMsg={handleEditMsg}
                 loggedInUserId={loggedInUserId}
                 selectionMode={selectedMsgs.length > 0}
                 currentGroup={currentGroup}
               />
-              
-            </React.Fragment>
-          );
-        })
-      ) : (
-          <NoMsgYet />
-
-      )}
-
-         
+            )}
+          </React.Fragment>
+        );
+      })}
 
       {typing && typingUserNames?.length > 0 && (
-        <div className="text-gray-500 italic text-sm mt-auto ">
-          {typingUserNames.length === 1 ? `${typingUserNames[0]} is typing...`
-            : typingUserNames.length === 2 ? `${typingUserNames[0]} and ${typingUserNames[1]} are typing...`
-              : `${typingUserNames[0]} and ${typingUserNames.length - 1} others are typing...`
-          }
+        <div className="text-gray-500 italic text-sm mt-auto">
+          {typingUserNames.length === 1
+            ? `${typingUserNames[0]} is typing...`
+            : typingUserNames.length === 2
+            ? `${typingUserNames[0]} and ${typingUserNames[1]} are typing...`
+            : `${typingUserNames[0]} and ${
+                typingUserNames.length - 1
+              } others are typing...`}
         </div>
       )}
 
@@ -299,4 +272,5 @@ const usersMap = useMemo(() => {
 };
 
 export default React.memo(GroupChatMessages);
+
 
