e4ecb83eb939ba1c0fc8bbd210645237a2474407
salman-shaikh-19 <salman.shaikh@differenzsystem.com>
2025-09-19 12:56:10 +0530

    Update ChatCommonLabel.jsx

--- src/common/components/ChatCommonLabel.jsx
+++ src/common/components/ChatCommonLabel.jsx
@@ -1,12 +1,14 @@
 import { uniqueId } from "lodash";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ChatCommonLabel = ({ children }) => {
+  const theme=useSelector(state=>state.common.theme);
   const id = uniqueId("chat-label-");
   // console.log(id);
    
   return (
-    <span id={id} className=" no-search-highlight text-gray-500 select-none my-2 px-3 py-1 rounded-md bg-white">
+    <span id={id} className={` no-search-highlight ${theme=='dark'?'text-gray-200 bg-gray-900':'text-gray-500 bg-white'} select-none my-2 px-3 py-1 rounded-md `}>
       {children}
     </span>
   );
